c131a7fb569686b387aeadf5be9c31fa110c79b5
Wanderson Soares <wandersonsoares841@gmail.com>
2025-02-26 23:19:52 -0300

    Add debug

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/user"
 
 	"github.com/Jonaires777/src/constants"
@@ -10,11 +11,27 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "debug" {
+		fmt.Println("Rodando modo debug...")
+		if err := filemanager.PrintSuperblock(); err != nil {
+			fmt.Println("Erro ao imprimir superbloco:", err)
+		}
+
+		if err := filemanager.PrintBitmap(); err != nil {
+			fmt.Println("Erro ao imprimir bitmap:", err)
+		}
+
+		if err := filemanager.PrintInodeTable(); err != nil {
+			fmt.Println("Erro ao imprimir tabela de inodes:", err)
+		}
+		return
+	}
+
 	if !filemanager.CheckFileExistence(constants.VirtualDisk) {
 		err := filemanager.CreateVirtualDisk(constants.VirtualDisk)
 		if err != nil {
 			panic(err)
-		} 
+		}
 	}
 
 	currentUser, err := user.Current()
